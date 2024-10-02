3e87717db96f5a8658ef2bcfef4765b55bd8dad1
Tanmayraut16 <Tanmayraut16@outlook.com>
2024-10-02 20:06:30 +0530

    portfolio

---
+++ src/assets/assets.js
@@ -0,0 +1,45 @@
+import c_logo from './c.png'
+import cpp from './c++.png'
+import canva from './canva.png'
+import css from './css.png'
+import firebase_logo from './firebase.png'
+import git from './git.png'
+import github from './github.png'
+import html_logo from './html.png'
+import java_logo from './javaP.png'
+import js from './js.png'
+import mongo from './mongo.png'
+import next from './next js.jpg'
+import node from './node.png'
+import postman from './postman.png'
+import ps from './problem_solving.png'
+import python from './python_logo.png'
+import react from './react.png'
+import sql from './sql.png'
+import tailwind from './tailwind.png'
+import vs from './vscode.png'
+
+const assets = {
+    c_logo,
+    cpp,
+    canva,
+    css,
+    firebase_logo,
+    git,
+    github,
+    html_logo,
+    java_logo,
+    js,
+    mongo,
+    next,
+    node,
+    postman,
+    ps,
+    python,
+    react,
+    sql,
+    tailwind,
+    vs
+}
+
+export default assets;
